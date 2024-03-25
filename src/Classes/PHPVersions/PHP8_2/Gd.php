a0a6b789d5b04202fcff31c0b23948a97717d497
Gorka <gorkau@gmail.com>
2024-03-25 18:25:29 +0100

    Corrected libraries to be added for gd extension

--- src/Classes/PHPVersions/PHP8_2/Gd.php
+++ src/Classes/PHPVersions/PHP8_2/Gd.php
@@ -2,11 +2,11 @@
 
 namespace Gorkau\DockerPhpGenerator\Classes\PHPVersions\PHP8_2;
 
-class Gd
+class Gd implements ModuleInterface
 {
     public function libInstallation() : string
     {
-        return "apt install -y libzip-dev zip";
+        return "apt install -y libjpeg-dev libpng-dev";
     }
 
     public function moduleConfiguration() : string
